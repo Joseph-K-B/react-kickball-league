21d81731a7cb3bd2d61c84b4ce77975906eeaab4
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-06 17:59:59 -0800

    player list rendering

--- src/views/PlayerList/PlayerList.jsx
+++ src/views/PlayerList/PlayerList.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { getPlayers } from "../../services/players"
+
 function PlayerList() {
+    // const { id } = useParams()
+    const[loading, setLoading] = useState(true)
+    const [players, setPlayers] = useState([])
+
+    useEffect(() => {
+        getPlayers()
+        .then((res) => setPlayers(res))
+        .finally(() => setLoading(false))
+    }, [])
+
+    if(loading) return <h1>Loading Players...</h1>
+
     return(
-        <h1>Player List</h1>
+        <>
+        <h1>Player List</h1><ul>
+            {players.map((player) => (
+                <li key={player.id}>
+                    <Link to={`/players/${player.id}`}>
+                        <p>{player.name}</p>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        </>
     )
 }
 
--- src/views/TeamList/TeamList.jsx
+++ src/views/TeamList/TeamList.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { getTeams } from '../../services/teams';
 
 function TeamList() {
-    const [loading, setLoading] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [teams, setTeams] = useState([]);
 
     useEffect(() => {
@@ -16,7 +16,8 @@ function TeamList() {
     
     return (
         <>
-        <h1>Team List</h1><ul>
+        <h1>Team List</h1>
+        <ul>
             {teams.map((team) => (
                 <li key={team.id}>
                     <Link to={`/teams/${team.id}`}>
